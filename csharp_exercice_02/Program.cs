4c4b44086563bcf75b9fff3fd8d7bc78a542e019
Julien Le Mee <julemee@gmail.com>
2024-05-22 00:47:42 -0400

    exercice 02

---
+++ csharp_exercice_02/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] valeurs = new string[3];
+
+for (int i = 0; i < 3; i++)
+{
+Console.WriteLine("Saisir la valeur n°" + (i + 1));
+  valeurs[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < 3; i++)
+{
+  Console.WriteLine($"La valeur {(i + 1)} est {valeurs[i]}");
+}
