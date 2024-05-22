76757d58dd55fb848bd2704469d2e40881e0e63c
Julien Le Mee <julemee@gmail.com>
2024-05-21 23:23:47 -0400

    exercice 01

---
+++ csharp_exercice_01/Program.cs
@@ -0,0 +1,20 @@
+﻿const int NbMystere = 7;
+
+Console.WriteLine("Saisir un nombre entre 1 et 10");
+int nombre = int.Parse(Console.ReadLine());
+
+if(nombre == NbMystere)
+{
+    Console.WriteLine("Bravo ! Vous avez trouvez le nombre mystère.");
+}
+else
+{
+    if(nombre < NbMystere)
+    {
+    Console.WriteLine("Le nombre mystère est plus grand...");
+    }
+    else
+    {
+    Console.WriteLine("Le nombre mystère est plus petit...");
+    }
+}
