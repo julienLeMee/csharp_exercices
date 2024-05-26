9453a3e416ae1a9d22934c923701ac964215dfe2
Julien Le Mee <julemee@gmail.com>
2024-05-26 12:10:29 -0400

    TPCalculatrice

--- TPCalculatrice/Program.cs
+++ TPCalculatrice/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TPCalculatrice;
+
+Console.WriteLine("#########################################");
+Console.WriteLine("##### Bienvenue sur ma calculatrice #####");
+Console.WriteLine("#########################################");
+Console.WriteLine();
+Console.WriteLine("## Saisissez le premier nombre ##");
+int nb1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("## Saisissez le deuxième nombre ##");
+int nb2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("# Saisissez l'opérateur (+ , - , * , / , %) #");
+string operateur = Console.ReadLine();
+
+// Calculatrice calc = new Calculatrice(); // ancienne syntax
+// var calc = new Calculatrice();
+Calculatrice calc = new();
+
+int result = 0;
+
+if (operateur == "+")
+{
+    result = calc.Addition(nb1, nb2);
+}
+else if (operateur == "-")
+{
+    result = calc.Soustraction(nb1, nb2);
+}
+else if (operateur == "*")
+{
+    result = calc.Multiplication(nb1, nb2);
+}
+else if (operateur == "/")
+{
+    result = calc.Division(nb1, nb2);
+}
+else if (operateur == "%")
+{
+    result = calc.Modulo(nb1, nb2);
+}
+else
+{
+    Console.WriteLine("Opérateur non reconnu");
+    return;
+}
+
+Console.WriteLine("####################################################");
+Console.WriteLine($"### Le résultat de votre opération est {result} ###");
+Console.WriteLine("####################################################");
